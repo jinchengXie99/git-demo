df5c3655aaa86c8b900b328f58f13365b19401a6
jinchengxie99 <=>
2022-06-16 20:52:17 +0800

    可以sleep的hello

---
+++ hello.2.0.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: hello.2.0.cpp
+	> Author: Jincheng Xie
+	> Mail: chengdiy@foxmail.com
+	> Created Time: 四  6/16 20:50:20 2022
+ ************************************************************************/
+
+#include<iostream>
+#include <unistd.h>
+using namespace std;
+int main ()
+{
+	int n = 3;
+	while(n--){
+	std::cout << "hello git " << std::endl;
+	sleep(1);
+	}
+}
