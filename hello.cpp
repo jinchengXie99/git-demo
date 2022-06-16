4ff0933187a5a633ab5ece90b26b1d86d25a667e
jinchengxie99 <=>
2022-06-16 19:59:49 +0800

    hello 1.0 version

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: Jincheng Xie
+	> Mail: chengdiy@foxmail.com
+	> Created Time: 四  6/16 19:58:07 2022
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int  main ()
+{
+	cout << "hello 1.0 version" << endl;
+}
